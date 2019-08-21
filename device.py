33fcf00d4b7ce11061796fe1845f0530d88e733d
Nicholas Stone <stone.nicholas@gmail.com>
2019-08-21 13:27:02 -0400

    Finished making SemiGutter devices work. Reorganized some code to reduce redundancy

--- device.py
+++ device.py
@@ -102,97 +102,12 @@ class RidgeSpec:
 
         seg=skimage.measure.label(seg>0,connectivity=1)
 
-        #fig,ax=plt.subplots()
-        #ax.imshow(skimage.color.label2rgb(seg))
-        #plt.show()
-        #filter out objects smaller than the ridges
-        obj_sizes=[(obj,seg[seg==obj].size) for obj in set(seg.ravel()) if obj>0]
-        max_size=max([obj_s[1] for obj_s in obj_sizes])
-        for obj,size in obj_sizes:
-            if size<0.8*max_size:
-                #this object is smaller than the ridges, remove from seg
-                seg[seg==obj]=0
+        seg=remove_edge_objs(seg)
 
-        #for each ridge, find the two points that are furthest apart, these will be our 'corners'
-        #Would probably be faster to first find the point furthest from the centroid, then the point furthest from that point
+        filter_small_objs(seg)
 
-        corners=[]
-        for ridge in set(seg[seg>0].ravel()):
-            #I think the data is in (y,x) order
-            this_ridge=np.zeros(seg.shape,seg.dtype)
-            y_coords,x_coords=np.where(seg==ridge)
-            this_ridge[y_coords,x_coords]=1
-            ridge_centroid=imp.get_centroid(this_ridge)
-            max_dist=0
-            first_corner=None
-            for a in zip(y_coords,x_coords):
-                #find furthest point from centroid
-                this_dist=np.sqrt(((ridge_centroid[0]-a[0])**2)+((ridge_centroid[1]-a[1])**2))
-                if this_dist>max_dist:
-                    first_corner=a
-                    max_dist=this_dist
-
-            max_dist=0
-            second_corner=None
-            for b in zip(y_coords,x_coords):
-                #find furthest point from first_corner
-                this_dist=np.sqrt(((first_corner[0]-b[0])**2)+((first_corner[1]-b[1])**2))
-                if this_dist>max_dist:
-                    second_corner=b
-                    max_dist=this_dist
-
-            corners.append([first_corner,second_corner])
-
-            #calculate ridge slope and reject objects that don't match the ridges
-            abs_slopes=[abs((c[0][0]-c[1][0])/(c[0][1]-c[1][1])) for c in corners]
-            med_slope=np.median(abs_slopes)
-            for i in range(len(corners)):
-                #if the slope is off by more than 20%, can it
-                if (abs_slopes[i]<(0.8*med_slope)) or (abs_slopes[i]>(1.2*med_slope)):
-                    del corners[i]
-
-
-        #Get 'top' corners and use them to estimate tilt, scale and number of ridges
-        top_corners=[]
-        for p1,p2 in corners:
-            #'top' corner has lower y value
-            top_c=p1 if p1[0]<p2[0] else p2
-            top_corners.append(top_c)
-
-        #fit a line to top corners to estimate tilt
-        popt,pcov=scipy.optimize.curve_fit(lambda x,m,b:[m*this_x+b for this_x in x],[p[1] for p in top_corners],[p[0] for p in top_corners],[0,top_corners[0][0]])
-        detected_slope=popt[0]
-        detected_intercept=popt[1]
-        detected_tilt=np.arctan(detected_slope)
-        print('detected_tilt:',detected_tilt)
-
-        #find closest distance between top corners to estimate ridge spacing and max distance to estimate number of ridges
-        min_dist=float('inf')
-        max_dist=-float('inf')
-        for c1 in top_corners:
-            for c2 in top_corners:
-                this_dist=np.sqrt(((c1[0]-c2[0])**2)+((c1[1]-c2[1])**2))
-                if this_dist>0:
-                    if this_dist>max_dist:
-                        max_dist=this_dist
-                    elif this_dist<min_dist:
-                        min_dist=this_dist
-
-        detected_ridge_spacing=min_dist
-        detected_num_ridges=int(np.round(max_dist/detected_ridge_spacing)+1)
-
-        #find coordinate of top corner with largest x component to get estimate of x offset, get y from linear fit
-        detected_x_offset=max([p[1] for p in top_corners])
-        detected_y_offset=int(np.round(detected_slope*detected_x_offset+detected_intercept))
-
-        #estimate scale factor from known device measurement
-        detected_scale=detected_ridge_spacing/device.spacing
-
-        #print('Estimated values:')
-        #print('tilt:{tilt}, spacing:{spacing}, number of ridges:{ridge}, y offset:{y}, x offset:{x}'.format(tilt=detected_tilt,spacing=detected_ridge_spacing,ridge=detected_num_ridges,y=detected_y_offset,x=detected_x_offset))
-        optimized_result=maximize_overlap(ridges_skeleton,device,[detected_num_ridges,detected_scale,detected_tilt,detected_y_offset,detected_x_offset,background.shape])
-        #need to add in detected_num_ridges and background.shape as they were not considered in optimization
-        optimized_params=[detected_num_ridges,*optimized_result.x,background.shape]
+        optimized_params,corners=find_ridges_in_seg_im(seg,device,ridges_skeleton)
+        
         if debug:
             device_mask=device.get_mask(*optimized_params)
             fig,ax=plt.subplots(nrows=2,ncols=2,sharex=True,sharey=True)
@@ -242,7 +157,7 @@ class RidgeSpecSemiGutter(RidgeSpec):
                 max_ratio_label=props.label
         print('area={} perimeter={}'.format(seg_props[max_ratio_label].area,seg_props[max_ratio_label].area))
         bounds=skimage.segmentation.find_boundaries(seg==max_ratio_label)
-        #find the channel area and look for convex corners
+        #find the channel area
         bounds_seg=skimage.measure.label(bounds+1,connectivity=1)
         bounds_seg_props=skimage.measure.regionprops(bounds_seg)
         max_area=-1
@@ -252,13 +167,6 @@ class RidgeSpecSemiGutter(RidgeSpec):
                 max_area=props.area
                 max_area_label=props.label
         channel=bounds_seg==max_area_label
-        #pick the line which cuts off the least of the 'ridge' side while resulting in the most objects
-        top_edge_sum=sum(channel[0,:])
-        bottom_edge_sum=sum(channel[-1,:])
-
-        if top_edge_sum>bottom_edge_sum:
-            #need to invert image to count up, will correct for this below
-            channel=channel[::-1,:]
 
         #now we want to work on the boundary of this image
         final_bounds=skimage.segmentation.find_boundaries(channel)
@@ -278,114 +186,14 @@ class RidgeSpecSemiGutter(RidgeSpec):
                     best_right=right
         rr,cc=skimage.draw.line(best_left,0,best_right,final_bounds.shape[1]-1)
         final_bounds[rr,cc]=1
-        if top_edge_sum>bottom_edge_sum:
-            #need to invert back
-            final_bounds=final_bounds[::-1,:]
 
         seg=skimage.measure.label(final_bounds+1,connectivity=1)
-        #remove all objects touching edges
-        edge_obj=[]
-        edge_obj.extend(seg[0,:])
-        edge_obj.extend(seg[:,0])
-        edge_obj.extend(seg[-1,:])
-        edge_obj.extend(seg[:,-1])
+        seg=remove_edge_objs(seg)
 
-        for obj in set(edge_obj):
-            seg[seg==obj]=0
+        filter_small_objs(seg)
 
-        seg=skimage.measure.label(seg>0,connectivity=1)
-
-        #fig,ax=plt.subplots()
-        #ax.imshow(skimage.color.label2rgb(seg))
-        #plt.show()
-        #filter out objects smaller than the ridges
-        obj_sizes=[(obj,seg[seg==obj].size) for obj in set(seg.ravel()) if obj>0]
-        max_size=max([obj_s[1] for obj_s in obj_sizes])
-        for obj,size in obj_sizes:
-            if size<0.8*max_size:
-                #this object is smaller than the ridges, remove from seg
-                seg[seg==obj]=0
-
-        #for each ridge, find the two points that are furthest apart, these will be our 'corners'
-        #Would probably be faster to first find the point furthest from the centroid, then the point furthest from that point
-
-        corners=[]
-        for ridge in set(seg[seg>0].ravel()):
-            #I think the data is in (y,x) order
-            this_ridge=np.zeros(seg.shape,seg.dtype)
-            y_coords,x_coords=np.where(seg==ridge)
-            this_ridge[y_coords,x_coords]=1
-            ridge_centroid=imp.get_centroid(this_ridge)
-            max_dist=0
-            first_corner=None
-            for a in zip(y_coords,x_coords):
-                #find furthest point from centroid
-                this_dist=np.sqrt(((ridge_centroid[0]-a[0])**2)+((ridge_centroid[1]-a[1])**2))
-                if this_dist>max_dist:
-                    first_corner=a
-                    max_dist=this_dist
-
-            max_dist=0
-            second_corner=None
-            for b in zip(y_coords,x_coords):
-                #find furthest point from first_corner
-                this_dist=np.sqrt(((first_corner[0]-b[0])**2)+((first_corner[1]-b[1])**2))
-                if this_dist>max_dist:
-                    second_corner=b
-                    max_dist=this_dist
-
-            corners.append([first_corner,second_corner])
-
-            #calculate ridge slope and reject objects that don't match the ridges
-            abs_slopes=[abs((c[0][0]-c[1][0])/(c[0][1]-c[1][1])) for c in corners]
-            med_slope=np.median(abs_slopes)
-            for i in range(len(corners)):
-                #if the slope is off by more than 20%, can it
-                if (abs_slopes[i]<(0.8*med_slope)) or (abs_slopes[i]>(1.2*med_slope)):
-                    del corners[i]
-
-
-        #Get 'top' corners and use them to estimate tilt, scale and number of ridges
-        top_corners=[]
-        for p1,p2 in corners:
-            #'top' corner has lower y value
-            top_c=p1 if p1[0]<p2[0] else p2
-            top_corners.append(top_c)
-
-        #fit a line to top corners to estimate tilt
-        popt,pcov=scipy.optimize.curve_fit(lambda x,m,b:[m*this_x+b for this_x in x],[p[1] for p in top_corners],[p[0] for p in top_corners],[0,top_corners[0][0]])
-        detected_slope=popt[0]
-        detected_intercept=popt[1]
-        detected_tilt=np.arctan(detected_slope)
-        print('detected_tilt:',detected_tilt)
-
-        #find closest distance between top corners to estimate ridge spacing and max distance to estimate number of ridges
-        min_dist=float('inf')
-        max_dist=-float('inf')
-        for c1 in top_corners:
-            for c2 in top_corners:
-                this_dist=np.sqrt(((c1[0]-c2[0])**2)+((c1[1]-c2[1])**2))
-                if this_dist>0:
-                    if this_dist>max_dist:
-                        max_dist=this_dist
-                    elif this_dist<min_dist:
-                        min_dist=this_dist
-
-        detected_ridge_spacing=min_dist
-        detected_num_ridges=int(np.round(max_dist/detected_ridge_spacing)+1)
-
-        #find coordinate of top corner with largest x component to get estimate of x offset, get y from linear fit
-        detected_x_offset=max([p[1] for p in top_corners])
-        detected_y_offset=int(np.round(detected_slope*detected_x_offset+detected_intercept))
-
-        #estimate scale factor from known device measurement
-        detected_scale=detected_ridge_spacing/device.spacing
-
-        #print('Estimated values:')
-        #print('tilt:{tilt}, spacing:{spacing}, number of ridges:{ridge}, y offset:{y}, x offset:{x}'.format(tilt=detected_tilt,spacing=detected_ridge_spacing,ridge=detected_num_ridges,y=detected_y_offset,x=detected_x_offset))
-        optimized_result=maximize_overlap(ridges_skeleton,device,[detected_num_ridges,detected_scale,detected_tilt,detected_y_offset,detected_x_offset,background.shape])
-        #need to add in detected_num_ridges and background.shape as they were not considered in optimization
-        optimized_params=[detected_num_ridges,*optimized_result.x,background.shape]
+        optimized_params,corners=find_ridges_in_seg_im(seg,device,ridges_skeleton)
+        
         if debug:
             device_mask=device.get_mask(*optimized_params)
             fig,ax=plt.subplots(nrows=2,ncols=3,sharex=True,sharey=True)
@@ -405,6 +213,35 @@ class RidgeSpecSemiGutter(RidgeSpec):
 
         return CalibratedDevice(device,*optimized_params[0:-1])
 
+    def get_mask(self,num_ridge:int,scale:float,tilt:float,offset_y:float,offset_x:float,im_shape:tuple,im_dtype=np.bool):
+        '''Get a binary mask showing the ridge geometry transformed into image coordinates
+        
+        -----Parameters-----
+        num_ridge: Number of ridges
+        scale: Microns to pixels conversion factor
+        tilt: Angle to rotate device geometry by
+        offset_y: Y offset in pixels
+        offset_x: X offset in pixels
+        im_shape: Tuple giving the desired shape of the returned mask
+        im_dtype: Desired dtype of the returned mask
+
+        -----Returns-----
+        mask: a binary image with the ridge geometry represented by one pixel wide positive lines'''
+        
+        mask=np.zeros(im_shape,im_dtype)
+        all_coords=self.get_ridge_coords(num_ridge,scale,tilt,offset_y,offset_x)
+        #generate mask
+        for ridge in all_coords:
+            ridge_x=[int(round(r[0])) for r in ridge]
+            ridge_y=[int(round(r[1])) for r in ridge]
+            lines_coords=[]
+            lines_coords.append(skimage.draw.line(ridge_y[0],ridge_x[0],ridge_y[1],ridge_x[1]))
+            lines_coords.append(skimage.draw.line(ridge_y[1],ridge_x[1],ridge_y[2],ridge_x[2]))
+            lines_coords.append(skimage.draw.line(ridge_y[3],ridge_x[3],ridge_y[0],ridge_x[0]))
+            for rr,cc in lines_coords:
+                mask[rr,cc]=True
+
+        return mask
     
 class CalibratedDevice:
     '''A class representing a sorting device calibrated to a specific video'''
@@ -589,3 +426,108 @@ def maximize_overlap(ridge_mask:np.ndarray,device:RidgeSpec,initial_guess:list):
     #optimize parameters other than num_ridges (initial_guess[0])
     res=scipy.optimize.minimize(lambda x: total_dist(ridge_mask,device.get_mask(initial_guess[0],*x,initial_guess[-1])),initial_guess[1:-1],method='Nelder-Mead')
     return res
+
+def remove_edge_objs(im):
+    #remove all objects touching edges
+    edge_obj=[]
+    edge_obj.extend(im[0,:])
+    edge_obj.extend(im[:,0])
+    edge_obj.extend(im[-1,:])
+    edge_obj.extend(im[:,-1])
+
+    for obj in set(edge_obj):
+        im[im==obj]=0
+
+    seg=skimage.measure.label(im>0,connectivity=1)
+    return im
+
+def filter_small_objs(seg):
+    #filter out objects smaller than the ridges
+    obj_sizes=[(obj,seg[seg==obj].size) for obj in set(seg.ravel()) if obj>0]
+    max_size=max([obj_s[1] for obj_s in obj_sizes])
+    for obj,size in obj_sizes:
+        if size<0.8*max_size:
+            #this object is smaller than the ridges, remove from seg
+            seg[seg==obj]=0
+def find_ridges_in_seg_im(seg,device,ridges_skeleton):
+    #for each ridge, find the two points that are furthest apart, these will be our 'corners'
+    #Would probably be faster to first find the point furthest from the centroid, then the point furthest from that point
+
+    corners=[]
+    for ridge in set(seg[seg>0].ravel()):
+        #I think the data is in (y,x) order
+        this_ridge=np.zeros(seg.shape,seg.dtype)
+        y_coords,x_coords=np.where(seg==ridge)
+        this_ridge[y_coords,x_coords]=1
+        ridge_centroid=imp.get_centroid(this_ridge)
+        max_dist=0
+        first_corner=None
+        for a in zip(y_coords,x_coords):
+            #find furthest point from centroid
+            this_dist=np.sqrt(((ridge_centroid[0]-a[0])**2)+((ridge_centroid[1]-a[1])**2))
+            if this_dist>max_dist:
+                first_corner=a
+                max_dist=this_dist
+
+        max_dist=0
+        second_corner=None
+        for b in zip(y_coords,x_coords):
+            #find furthest point from first_corner
+            this_dist=np.sqrt(((first_corner[0]-b[0])**2)+((first_corner[1]-b[1])**2))
+            if this_dist>max_dist:
+                second_corner=b
+                max_dist=this_dist
+
+        corners.append([first_corner,second_corner])
+
+        #calculate ridge slope and reject objects that don't match the ridges
+        abs_slopes=[abs((c[0][0]-c[1][0])/(c[0][1]-c[1][1])) for c in corners]
+        med_slope=np.median(abs_slopes)
+        for i in range(len(corners)):
+            #if the slope is off by more than 20%, can it
+            if (abs_slopes[i]<(0.8*med_slope)) or (abs_slopes[i]>(1.2*med_slope)):
+                del corners[i]
+
+
+    #Get 'top' corners and use them to estimate tilt, scale and number of ridges
+    top_corners=[]
+    for p1,p2 in corners:
+        #'top' corner has lower y value
+        top_c=p1 if p1[0]<p2[0] else p2
+        top_corners.append(top_c)
+
+    #fit a line to top corners to estimate tilt
+    popt,pcov=scipy.optimize.curve_fit(lambda x,m,b:[m*this_x+b for this_x in x],[p[1] for p in top_corners],[p[0] for p in top_corners],[0,top_corners[0][0]])
+    detected_slope=popt[0]
+    detected_intercept=popt[1]
+    detected_tilt=np.arctan(detected_slope)
+    print('detected_tilt:',detected_tilt)
+
+    #find closest distance between top corners to estimate ridge spacing and max distance to estimate number of ridges
+    min_dist=float('inf')
+    max_dist=-float('inf')
+    for c1 in top_corners:
+        for c2 in top_corners:
+            this_dist=np.sqrt(((c1[0]-c2[0])**2)+((c1[1]-c2[1])**2))
+            if this_dist>0:
+                if this_dist>max_dist:
+                    max_dist=this_dist
+                elif this_dist<min_dist:
+                    min_dist=this_dist
+
+    detected_ridge_spacing=min_dist
+    detected_num_ridges=int(np.round(max_dist/detected_ridge_spacing)+1)
+
+    #find coordinate of top corner with largest x component to get estimate of x offset, get y from linear fit
+    detected_x_offset=max([p[1] for p in top_corners])
+    detected_y_offset=int(np.round(detected_slope*detected_x_offset+detected_intercept))
+
+    #estimate scale factor from known device measurement
+    detected_scale=detected_ridge_spacing/device.spacing
+
+    #print('Estimated values:')
+    #print('tilt:{tilt}, spacing:{spacing}, number of ridges:{ridge}, y offset:{y}, x offset:{x}'.format(tilt=detected_tilt,spacing=detected_ridge_spacing,ridge=detected_num_ridges,y=detected_y_offset,x=detected_x_offset))
+    optimized_result=maximize_overlap(ridges_skeleton,device,[detected_num_ridges,detected_scale,detected_tilt,detected_y_offset,detected_x_offset,ridges_skeleton.shape])
+    #need to add in detected_num_ridges and background.shape as they were not considered in optimization
+    optimized_params=[detected_num_ridges,*optimized_result.x,ridges_skeleton.shape]
+    return optimized_params,corners
--- devlib.py
+++ devlib.py
@@ -2,4 +2,4 @@ from heimdall.device import RidgeSpec as Ridge
 from heimdall.device import RidgeSpecSemiGutter as SemiGutter
 import math
 dev_a=Ridge(ridge_height=400,ridge_width=40,ridge_spacing=240,ridge_angle=math.pi/6)
-test=SemiGutter(ridge_height=400,ridge_width=40,ridge_spacing=240,ridge_angle=math.pi/6)
+dev_2=SemiGutter(ridge_height=480, ridge_width=40, ridge_spacing=240, ridge_angle=-math.pi/6)
